50e45b1eb4e30b9977b2f54608a36e6393ec3927
rochelletham <rochelletham@Rochelles-MacBook-Pro.local>
2025-05-01 11:39:01 -0700

    Updating kitchensink slider

--- kitchensink/kitchensink.js
+++ kitchensink/kitchensink.js
@@ -3,7 +3,6 @@ const sliderValue = document.querySelector('#slider-value');
 
 slider.addEventListener('input', function () {
     var val = ((this.value - this.min) / (this.max - this.min)) * 100; // Calculate percentage)
-    console.log(val); // Log the value to the console
     const percentage = Math.round(100 - (val / this.max) * 100); // Reverse the percentage calculation
     console.log("percentage: ", percentage);
     slider.style.setProperty('--slider-value', `${percentage}%`);
