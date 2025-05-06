88e4e04f32c5e17d3dc4f254fffd613891516784
rochelletham <rochelletham@Rochelles-MacBook-Pro.local>
2025-05-06 16:51:26 -0700

    Implemented keyboard editable text for vertical slider

--- kitchensink/kitchensink.js
+++ kitchensink/kitchensink.js
@@ -1,14 +1,23 @@
-const vertslider = document.querySelector('.vert-slider');
-const vertsliderValue = document.querySelector('#vert-slider-value');
+const vertSlider = document.querySelector('#vertical-slider');
+const vertSliderValue = document.querySelector('#vert-slider-value');
 
-vertslider.addEventListener('input', function () {
-    var val = ((this.value - this.min) / (this.max - this.min)) * 100; // Calculate percentage)
-    const percentage = Math.round(100 - (val / this.max) * 100); 
-    console.log("percentage: ", percentage);
-    vertslider.style.setProperty('--vert-slider-value', `${percentage}%`);
-    vertsliderValue.textContent = percentage; // updates the displayed value for slider
-    // Update the track's background
-    this.style.background = `linear-gradient(to top, rgba(7, 235, 123, 1) ${percentage}%, rgba(38, 42, 45, 1) ${percentage}%)`; // Update background
+vertSlider.addEventListener('input', function () {
+    // Synchronize input number with slider value
+    vertSliderValue.value = vertSlider.value; 
+    this.style.background = `linear-gradient(to top, rgba(7, 235, 123, 1) ${vertSlider.value}%,
+     rgba(38, 42, 45, 1) ${vertSlider.value}%)`;
+});
+
+
+// Update the slider when the number input changes
+vertSliderValue.addEventListener('input', function () {
+    let value = parseInt(vertSliderValue.value, 10);
+      if (value >= 0 && value <= 100) {
+        vertSlider.value = value;
+        vertSliderValue.value = vertSlider.value;
+        vertSlider.style.background = `linear-gradient(to top, rgba(7, 235, 123, 1) ${vertSlider.value}%,
+     rgba(38, 42, 45, 1) ${vertSlider.value}%)`;
+      }
 });
 
 const horislider = document.querySelector('.hori-slider');
