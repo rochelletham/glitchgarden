678a219942538f85b2ddb9f952c7148b891fc010
rochelletham <rochelletham@Rochelles-MacBook-Pro.local>
2025-05-12 00:56:53 -0700

    Fixed horizontal slider textbox and ticks

--- kitchensink/kitchensink.js
+++ kitchensink/kitchensink.js
@@ -20,15 +20,29 @@ vertSliderValue.addEventListener('input', function () {
       }
 });
 
-const horislider = document.querySelector('.hori-slider');
-const horisliderValue = document.querySelector('#hori-slider-value');
+const horiSlider = document.querySelector('#hori-slider');
+const horiSliderValue = document.querySelector('#hori-slider-value');
 
-horislider.addEventListener('input', function () {
-    var val = ((this.value - this.min) / (this.max - this.min)) * 100; // Calculate percentage)
-    const percentage = Math.round((val / this.max) * 100); // Reverse the percentage calculation
-    console.log("percentage: ", percentage);
-    horislider.style.setProperty('--hori-slider-value', `${percentage}%`);
-    horisliderValue.textContent = percentage; // updates the displayed value for slider
-    // Update the track's background
-    this.style.background = `linear-gradient(to right, rgba(7, 235, 123, 1) ${percentage}%, rgba(38, 42, 45, 1) ${percentage}%)`; // Update background
+horiSlider.addEventListener('input', function () {
+    // Synchronize input number with slider value
+    horiSliderValue.value = horiSlider.value; 
+    this.style.background = `linear-gradient(to right, rgba(7, 235, 123, 1) ${horiSlider.value}%,
+     rgba(38, 42, 45, 1) ${horiSlider.value}%)`;
+});
+
+horiSlider.addEventListener('input', function () {
+  let value = parseInt(horiSliderValue.value, 10);
+    if (value >= 0 && value <= 100) {
+      horiSlider.value = value;
+      horiSliderValue.value = horiSlider.value;
+      horiSlider.style.background = `linear-gradient(to right, rgba(7, 235, 123, 1) ${horiSlider.value}%,
+    rgba(38, 42, 45, 1) ${horiSlider.value}%)`;
+    }
+    // var val = ((this.value - this.min) / (this.max - this.min)) * 100; // Calculate percentage)
+    // const percentage = Math.round((val / this.max) * 100); // Reverse the percentage calculation
+    // console.log("percentage: ", percentage);
+    // horislider.style.setProperty('--hori-slider-value', `${percentage}%`);
+    // horisliderValue.textContent = percentage; // updates the displayed value for slider
+    // // Update the track's background
+    // this.style.background = `linear-gradient(to right, rgba(7, 235, 123, 1) ${percentage}%, rgba(38, 42, 45, 1) ${percentage}%)`; // Update background
 });
