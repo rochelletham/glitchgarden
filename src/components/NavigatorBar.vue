cfb95f6c500e8ac02aac66a5bb671077ad6d1863
rochelletham <rochelletham@Rochelles-MacBook-Pro.local>
2025-05-14 14:12:56 -0700

    Inprogress dark theme

--- src/components/NavigatorBar.vue
+++ src/components/NavigatorBar.vue
@@ -1,21 +1,27 @@
 <template>
-    <nav class="bg-gray-900 p-4 w-full fixed top-0">
+    <nav class="p-4 w-full fixed top-0">
         <div class="container mx-auto flex items-center justify-between">
             <div class="fixed left-8">
-                <a href="/" class="text-white text-2xl font-semibold whitespace-nowrap">GlitchGarden</a>
+                <a href="/" class="text-t-color text-2xl font-semibold whitespace-nowrap ml-10 hover:text-opacity-75">GlitchGarden</a>
             </div>
-            <div class="hidden md:flex space-x-8 ml-auto">
-                <!-- <a href="" class="text-white hover:bg-gray-700 px-3 py-2 rounded-md">About</a> -->
-                <router-link :to="{ path: '/', query: { component: 'About' } }" class="text-white hover:bg-gray-700 px-3 py-2 rounded-md">About</router-link>
+            <div class="text-t-color md:flex space-x-8 ml-auto hover:text-opacity-75">
+                <router-link :to="{ path: '/', query: { component: 'About' } }" class="text-t-color  px-3 py-2 -mr-7 hover:text-opacity-75">About</router-link>
                 <div class="relative"  @mouseleave="hideDropdown">
-                        <button @mouseover="showDropdown" class="text-white hover:bg-gray-700 px-3 py-2 rounded-md">Other Exercises</button>
-                        <div v-show="isDropdownVisible" @mouseover="showDropdown" class="absolute left-0 top-full pt-0 py-2 w-48 bg-gray-700 rounded-md shadow-xl z-10">
+                        <button @mouseover="showDropdown" class="text-t-color d px-3 py-2 -ml-1 hover:text-opacity-75">Other Exercises</button>
+                        <div v-show="isDropdownVisible" @mouseover="showDropdown" class="absolute left-0 top-full pt-0 py-2 w-48 bg-secondary-bk rounded-md shadow-xl z-10">
+                            <router-link :to="{ path: '/', query: { component: 'FlangerContent' } }" class="block text-left text-white hover:bg-tertiary-bk px-3 py-2 rounded-md">Flanger</router-link>
+                            <router-link :to="{ path: '/', query: { component: 'PhaserContent' } }" class="block text-left text-white hover:bg-tertiary-bk px-3 py-2 rounded-md">Phaser</router-link>
+                            <router-link :to="{ path: '/', query: { component: 'ChorusContent' } }" class="block text-left text-white hover:bg-tertiary-bk px-3 py-2 rounded-md">Chorus</router-link>
+                            <router-link :to="{ path: '/', query: { component: 'DoublingContent' } }" class="block text-left text-white hover:bg-gray-600 px-3 py-2 rounded-md">Doubling</router-link>
+                            <router-link :to="{ path: '/', query: { component: 'EchoContent' } }" class="block text-left text-white hover:bg-gray-600 px-3 py-2 rounded-md">Echo</router-link>
+                        </div>
+                        <!-- <div v-show="isDropdownVisible" @mouseover="showDropdown" class="absolute left-0 top-full pt-0 py-2 w-48 bg-gray-700 rounded-md shadow-xl z-10">
                             <router-link :to="{ path: '/', query: { component: 'FlangerContent' } }" class="block text-left text-white hover:bg-gray-600 px-3 py-2 rounded-md">Flanger</router-link>
                             <router-link :to="{ path: '/', query: { component: 'PhaserContent' } }" class="block text-left text-white hover:bg-gray-600 px-3 py-2 rounded-md">Phaser</router-link>
                             <router-link :to="{ path: '/', query: { component: 'ChorusContent' } }" class="block text-left text-white hover:bg-gray-600 px-3 py-2 rounded-md">Chorus</router-link>
                             <router-link :to="{ path: '/', query: { component: 'DoublingContent' } }" class="block text-left text-white hover:bg-gray-600 px-3 py-2 rounded-md">Doubling</router-link>
                             <router-link :to="{ path: '/', query: { component: 'EchoContent' } }" class="block text-left text-white hover:bg-gray-600 px-3 py-2 rounded-md">Echo</router-link>
-                        </div>
+                        </div> -->
                     </div>
                 </div>
         </div>
@@ -58,8 +64,7 @@ nav {
     width: 100%;
     height: auto;
     z-index: 50;
-    /* height: 4rem; 64px, matches h-16 */
-    
+    background-color: theme('colors.secondary-bk');   
 }
 .dropdown-menu {
     opacity: 0;
@@ -71,4 +76,5 @@ nav {
     opacity: 1;
     visibility: visible;
 }
+
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,21 +2,51 @@
 module.exports = {
   content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    // colors: {
-      // transparent: 'transparent',
-      // current: 'currentColor',
-      // 'lightgreen': '#86efac',
-      // 'white': '#ffffff',
-      // 'purple': '#3f3cbb',
-      // 'midnight': '#121063',
-      // 'metal': '#565584',
-      // 'tahiti': '#3ab7bf',
-      // 'silver': '#ecebff',
-      // 'bubble-gum': '#ff77e9',
-      // 'bermuda': '#78dcca',
-    // },
-    extend: {},
-  },
-  plugins: [],
-}
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'lightgreen': '#86efac',
+        'white': '#ffffff',
+        'purple': '#3f3cbb',
+        'midnight': '#121063',
+        'metal': '#565584',
+        'tahiti': '#3ab7bf',
+        'silver': '#ecebff',
+        'bubble-gum': '#ff77e9',
+        'bermuda': '#78dcca',
 
+        'background-color':'#1b1d1f',        /* near-black dark gray */
+        'surface-color': '#25282a',           /* card/panel surface */
+        'border-color': '#2f3234',            /* subtle borders */
+
+        // text colors
+        't-color': 'rgba(236, 246, 242, 1)',
+        'sec-t-color': 'rgb(228, 239, 235)',
+        'muted-text-color': '#999999',
+
+        /* Kiwi Green Accent */
+        'dark-green': 'rgba(138, 200, 134, 1)',
+        'light-green': 'rgba(148, 212, 122, 1)',
+        'orange':'rgba(246, 177, 81, 1)',
+        'light-blue':'rgba(123, 161, 233, 1)',
+        'purple':'rgba(123, 161, 233, 1)',
+
+        /* background colors */
+        'primary-bk': 'rgb(37, 37, 37)',
+        'secondary-bk': 'rgb(52, 52, 52)',
+        'tertiary-bk': 'rgba(87, 94, 103, 1)',
+        // 'tertiary-bk': 'rgb(56, 59, 65)',
+        'primary-color': '#85cb33',
+        'primary-color-hover': '#6dae29',
+        'primary-color-muted': 'rgba(133, 203, 51, 0.2)',
+
+        /* Inputs & UI */
+        'input-bg-color': '#2c2f31',
+        'input-border-color': '#3c3f41',
+        'button-text-color': '#0a0a0a'
+      },
+    },
+  plugins: [],
+  }
+}
