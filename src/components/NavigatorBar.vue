b797da795a7d58ef219aebbfa0efa969537b8ab1
rochelletham <rochelle.tham@gmail.com>
2025-10-08 18:33:37 -0700

    Adjusted logo to match color scheme and adapted sliders for mobile

--- src/components/NavigatorBar.vue
+++ src/components/NavigatorBar.vue
@@ -2,14 +2,14 @@
     <nav class="p-4 w-full fixed top-0">
         <div class="container mx-auto flex items-center justify-between">
             <div class="justify-start h-auto flex">
-                <a href="/" class="w-[25%] h-[25%] object-left -mt-5 hover:opacity-75"><img src="../svgs/sproutlogo.svg"></a>
-                <a href="/" class="mt-4 ml-2 text-2xl text-light-green font-semibold hover:text-opacity-75">GlitchGarden</a>
+                <a href="/" class="w-[30%] mt-4 object-left hover:opacity-75"><img src="../svgs/sproutlogo.gif"></a>
+                <a href="/" class="mt-4 text-2xl ml-1 text-light-green font-semibold hover:text-opacity-75">GlitchGarden</a>
             </div>
 
-            <div class="text-t-color md:flex space-x-8 ml-auto hover:text-t-color">
-                <router-link :to="{ path: '/', query: { component: 'About' } }" class="text-light-green  px-3 py-2 -mr-7 hover:text-t-color">About</router-link>
+            <div class="text-t-color flex space-x-8 ml-auto hover:text-t-color">
+                <router-link :to="{ path: '/', query: { component: 'About' } }" class="text-light-green flex px-3 py-2 -mr-7 hover:text-t-color">About</router-link>
                 <div class="relative"  @mouseleave="hideDropdown">
-                        <button @mouseover="showDropdown" class="text-light-green d px-3 py-2 -ml-1 hover:text-t-color">Other Exercises</button>
+                        <button @mouseover="showDropdown" class="text-light-green flex px-3 py-2 -ml-1 hover:text-t-color">Other Exercises</button>
                         <div v-show="isDropdownVisible" @mouseover="showDropdown" class="absolute left-0 top-full pt-0 py-2 w-48 bg-secondary-bk rounded-md shadow-xl z-10">
                             <router-link :to="{ path: '/', query: { component: 'FlangerContent' } }" class="block text-left text-light-green hover:bg-tertiary-bk px-3 py-2 rounded-md" @click.native="pageChange">Flanger</router-link>
                             <router-link :to="{ path: '/', query: { component: 'EchoContent' } }" class="block text-left text-light-green hover:bg-gray-600 px-3 py-2 rounded-md" @click.native="pageChange">Echo</router-link>
--- src/ui-components/HorizontalSlider.js
+++ src/ui-components/HorizontalSlider.js
@@ -53,13 +53,23 @@ export class HorizontalSlider extends HTMLElement {
         display: flex;
         flex-direction: row;
         align-items: center;
-        width: 480px;
-        height: 40px;
+        width: 75%;
+        height: 75%;
+        max-width: 480px;
+        max-height: 40px;
         gap: 5px; 
         position: relative;
-        padding: 10px;
-        padding-top: 5px;
-        margin-bottom: 10px;
+        padding: 2%;
+        margin-bottom: 1%;
+
+        // using % instead to accomodate for mobile view
+        // width: 480px;
+        // height: 40px;
+        // gap: 5px; 
+        // position: relative;
+        // padding: 10px;
+        // padding-top: 5px;
+        // margin-bottom: 10px;
       
       }
 
@@ -102,8 +112,10 @@ export class HorizontalSlider extends HTMLElement {
       .hori-slider::-webkit-slider-thumb {
         -webkit-appearance: none; 
         appearance: none;
-        height: 33px; 
-        width: 12px;    /* because track width is 12px*/
+        // width: 12px;    /* because track width is 12px*/
+        width: 3%;
+        min-height: 33px; 
+        min-width: 12px; /* minimum width for better usability */
         border: 1px solid rgb(255, 255, 255);
         border-radius: 3px;
         cursor: pointer; /* Pointer on hover */
