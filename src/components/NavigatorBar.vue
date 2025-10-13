04f5e9614e19361d7c8a2ef35ac8203da1a1a8b2
rochelletham <rochelle.tham@gmail.com>
2025-10-13 15:02:54 -0700

    Fixed logo left alignment in nav bar

--- src/components/NavigatorBar.vue
+++ src/components/NavigatorBar.vue
@@ -1,9 +1,9 @@
 <template>
-    <nav class="p-4 w-full fixed top-0">
+    <nav class="p-4 w-full fixed top-0 pl-5">
         <div class="container mx-auto flex items-center justify-between">
-            <div class="justify-start h-auto flex">
-                <a href="/" class="w-[25%] h-[25%] object-left -mt-5 hover:opacity-75"><img src="../svgs/sproutlogo.svg"></a>
-                <a href="/" class="mt-4 ml-2 text-2xl text-light-green font-semibold hover:text-opacity-75">GlitchGarden</a>
+            <div class="justify-start h-auto float-left flex flex-row items-left ">
+                <a href="/" class="w-[20%] h-[20%] mt-3.5 -ml-2 hover:opacity-75"><img src="../svgs/sproutlogo.gif"></a>
+                <a href="/" class="mt-4 text-2xl text-light-green font-semibold hover:text-opacity-75">GlitchGarden</a>
             </div>
 
             <div class="text-t-color md:flex space-x-8 ml-auto hover:text-t-color">
@@ -23,7 +23,7 @@
         </div>   
     </nav>
     <div class="h-16"></div>
-    <div class="h-6 bg-light-green"></div>
+    <div class="h-5 bg-light-green"></div>
 </template>
 
 
